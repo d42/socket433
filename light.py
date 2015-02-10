94e9f0951704919038c3d8859f7d516370f491cd
dzikie drożdże <daz@hackerspace.pl>
2015-02-10 23:50:28 +0100

    more python

--- light.py
+++ light.py
@@ -1,8 +1,26 @@
-import serial
-from time import sleep
+#!/usr/bin/env python3
+import argparse
+import sys
+from itertools import chain
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
-ser.write(b"\x30")
-sleep(1)
-ser.write(b"\x31")
-sleep(1)
+import serial
+
+
+def parse_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start", type=int, nargs="+", default=())
+    parser.add_argument("--stop", type=int, nargs="+", default=())
+
+    return parser.parse_args(sys.argv[1:])
+
+
+def main():
+    options = parse_options()
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+
+    for i in chain([i << 4 | 1 for i in options.start],
+                   [i << 4 | 0 for i in options.stop]):
+        ser.write(i.to_bytes(1, 'little'))
+
+if __name__ == '__main__':
+    main()
